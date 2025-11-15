f25e2dca3bc7aa80e2c483bc0d14535544fcc052
Isaac Malak <isaacmalak123@gmail.com>
2025-11-15 21:17:55 +0200

    feat: add home page file to a more structured folder, add prettier and prettier plugin for tailwindcss, implement explore button

---
+++ app/home/components/ExploreButton.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export function ExploreButton() {
+  return (
+    <button
+      onClick={() => {}}
+      type="button"
+      id="explore-btn"
+      className="cursor-pointer font-bold p-3 text-center border-1 border-white rounded-3xl m-5 w-1/4 bg-gray-500/10 hover:bg-gray-700"
+    >
+      <a> Explore More </a>
+    </button>
+  );
+}
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,10 @@
 import { Html } from "next/document";
 import Image from "next/image";
+import { ExploreButton } from "./components/ExploreButton";
 
 export default function Home() {
   return (
-    <section className="flex justify-center items-center min-w-screen min-h-screen bg-transparent dark:bg-transparent">
+    <section className="flex flex-col justify-center items-center w-full h-full bg-transparent dark:bg-transparent">
       <h1 className="text-center">
         The Hub for every developing events <br />
         Event you can't miss
@@ -11,6 +12,7 @@ export default function Home() {
       <p className="text-center">
         Hackathons, Meetups, and conferences. All in one place
       </p>
+      <ExploreButton />
     </section>
   );
 }
