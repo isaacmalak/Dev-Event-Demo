2bf716f70c680793e13fea174bdebdd2e48f5fb2
Isaac Malak <isaacmalak123@gmail.com>
2025-11-16 18:08:19 +0200

    fix: update project name in package.json to match the application
    feat: add Home nav bar

--- app/home/components/NavBar.tsx
+++ app/home/components/NavBar.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export function HomeNavBar(){
-    return <header> 
-
-        <nav> 
-            <Link href='/' className="logo" > 
-            <Image src='' alt=""> 
-            </Image>
-            </Link>
-        </nav>
-    </header>;
+export function HomeNavBar() {
+  return (
+    <header>
+      <nav>
+        <Link href="/" className="logo">
+          <Image src="/icons/logo.png" alt="logo" />
+          <p>Dev Events</p>
+          <ul>
+            <Link href="/"> Home</Link>
+            <Link href="/events"> Events</Link>
+            <Link href="/events-form"> Create Event </Link>
+          </ul>
+        </Link>
+      </nav>
+    </header>
+  );
 }
