95080c347f0f0584c4e76b558850a3f1d103e42c
Isaac Malak <isaacmalak123@gmail.com>
2025-11-25 15:44:25 +0200

    feat: implement email submission form in BookEvent component

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -1,9 +1,37 @@
-export function BookEvent() {
-    return (
-        <div className="booking">
-            <p className="text-lg font-semibold"> Book Event</p>
-        </div>
-    );
+"use client";
+import { useState } from "react";
 
-    
+export function BookEvent() {
+  const [email, setEmail] = useState("");
+  const [isSubmitted, setSubmission] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setTimeout(() => {
+      setSubmission(true);
+    }, 1000);
+  };
+  return (
+    <div id="book-event">
+      {isSubmitted ? (
+        <p className="text-sm"> Thank you for signing up</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email Address </label>
+            <input
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="Enter your email address"
+            />
+            <button type="submit" className="button-submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
 }
