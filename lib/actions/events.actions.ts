167f70c816770fe2ac1d1e8838bfe8c774586b88
Isaac Malak <isaacmalak123@gmail.com>
2025-11-25 15:44:36 +0200

    feat: add function to retrieve similar events by title

---
+++ lib/actions/events.actions.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { Event } from "@/database";
+import { connectToDatabase } from "../mongodb";
+
+export async function getSimilarEventsByTitle(title: string) {
+  try {
+    await connectToDatabase();
+    const event = await Event.findOne({ title });
+    return await Event.find({
+      tags: { $in: event?.tags || [] },
+      _id: { $ne: event?._id },
+    })
+      .limit(3)
+      .exec();
+  } catch {
+    return [];
+  }
+}
